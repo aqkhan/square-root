d8a4b35fc7ff09f6157a604fc241029a40b71e61
AK <aqk.neo@gmail.com>
2024-01-17 20:29:44 +0500

    optimized for speed

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,26 @@ fn generate_random_number(min: i32, max: i32, old_value: Option<i32>) -> i32 {
 }
 
 fn square_root(input: i32) -> i32 {
-    let mut random_number = generate_random_number(1, input, None);
+    let mut min = 1;
+    let mut max = input;
+    let mut random_number = generate_random_number(min, max, None);
     let mut iteration = 0;
     while (random_number * random_number) != input {
         iteration = iteration + 1;
-        println!("Iteration number: {} | Random number: {}", iteration, random_number);
-        random_number = generate_random_number(1, input, Some(random_number));
+        println!("Min {} Max {} for iteration no: {} || Random number: {}", min, max, iteration, random_number);
+        if random_number * random_number > input {
+            max = random_number;
+        }
+        else {
+            min = random_number
+        }
+        // println!("Iteration number: {} | Random number: {}", iteration, random_number);
+        random_number = generate_random_number(min, max, Some(random_number));
     }
     return random_number
 }
 
 fn main() {
-    let input = 81;
+    let input = 1024;
     println!("The square root of {} is {}", input, square_root(input));
 }
